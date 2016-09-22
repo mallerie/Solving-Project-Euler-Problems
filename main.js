e1308df05a8201571f9807b7737250e0f1cb1101
Mallerie Shirley <mallerie@gmail.com>
2016-09-21 22:00:14 -0400

    Deleted extra ending entries

--- main.js
+++ main.js
@@ -35,12 +35,14 @@ var total = 0;
 
 function evenFibonacciTotal() {
 
-  for (var i = 0; i < 36; i++) {
+  for (var i = 0;; i++) {
     if (i === 0) {
     arr.push(i);
     } else if (i === 1) {
     arr.push(i);
-    }  else if (arr[i] > 4000000) {
+    } else if (arr[i] > 4000000) {
+      arr.pop();
+      arr.pop();
       break;
     } else if (i >= 2 ) {
       arr[i] = ( arr[i - 2] ) + ( arr[i - 1] );
